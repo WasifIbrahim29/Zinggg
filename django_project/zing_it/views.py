ac9cbaec5f89887102572475df6cb5b38fcd6b85
Wasif <wasif.ibrahim29@gmail.com>
2020-01-23 16:46:21 +0500

    Initial commit

--- django_project/zing_it/views.py
+++ django_project/zing_it/views.py
@@ -4,15 +4,8 @@ from django.http import HttpResponse
 # Create your views here.
 
 def home(request):
-    biggest_hits_2018 = [
-            {"id": 1, "Track": "thank u, next", "Artist": "Ariana Grande", "Album": "thank u, next", "Length": "3:27"},
-            {"id": 2, "Track": "One Kiss, next", "Artist": "Dua Lipa, Calvin Harris", "Album": "One Kiss", "Length": "3:34"},
-            {"id": 3, "Track": "Better Now", "Artist": "Post Malone", "Album": "beerbongs & bentleys", "Length": "3:51"},
-            {"id": 4, "Track": "The Middle", "Artist": "Grey,Marren Morris, ZEDD", "Album": "The Middle", "Length": "3:04"},
-            {"id": 5, "Track": "Love Lies", "Artist": "Normani, Khalid", "Album": "Love Lies", "Length": "3:21"},
-            {"id": 6, "Track": "Rise", "Artist": "Jack & Jack, Jonas Blue", "Album": "Blue", "Length": "3:14"},
-        ]
-    return render(request,'zing_it/home.html',{"songs":biggest_hits_2018})
+    music_types=['Pop','Rock','R&B','Soul & Funk','Blues','Reggae','Soundtracks','Dance & EDM','Rap', 'Asian Music','Jazz','Kpop','Metal','Electronic','Classical']
+    return render(request,'zing_it/home.html',{"music_types":music_types})
 
 
 def about(request):
