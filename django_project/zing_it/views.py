22cd6dd9545a843a6f783a6386ea7e7e0681612d
Your Name <you@example.com>
2020-05-15 16:07:30 +0500

    initial commit

--- django_project/zing_it/views.py
+++ django_project/zing_it/views.py
@@ -3,6 +3,8 @@ from django.http import HttpResponse
 import datetime
 from django.http import Http404
 
+from .forms import Signup
+
 # Create your views here.
 
 
@@ -12,8 +14,8 @@ my_songs = [
             {"id": 2, "Track": "One Kiss, next", "Artist": "Dua Lipa, Calvin Harris", "Album": "One Kiss", "Length": "3:34","playlist_id": 1},
             {"id": 3, "Track": "Better Now", "Artist": "Post Malone", "Album": "beerbongs & bentleys", "Length": "3:51","playlist_id": 1},
             {"id": 4, "Track": "The Middle", "Artist": "Grey,Marren Morris, ZEDD", "Album": "The Middle", "Length": "3:04","playlist_id": 1},
-            {"id": 5, "Track": "Love Lies", "Artist": "Normani, Khalid", "Album": "Love Lies", "Length": "3:21","playlist_id": 1},
-            {"id": 6, "Track": "Rise", "Artist": "Jack & Jack, Jonas Blue", "Album": "Blue", "Length": "3:14","playlist_id": 1},
+            {"id": 5, "Track": "Love Lies", "Artist": "Normani, Khalid", "Album": "Love Lies", "Length": "3:21","playlist_id": 2},
+            {"id": 6, "Track": "Rise", "Artist": "Jack & Jack, Jonas Blue", "Album": "Blue", "Length": "3:14","playlist_id": 2},
         ]
 
 my_playlists=[
@@ -47,4 +49,16 @@ def playlist(request,id):
             songs.append(song)
         
     return render(request,'zing_it/songs.html',{"songs":songs,"playlist_name": playlist_name})
+
+def signup(request):
+    form = Signup(request.POST or None)
+    status= " "
+    if form.is_valid():
+        password= form.cleaned_data.get("password")
+        confirm_password = form.cleaned_data.get("confirm_password")
+        if(password!= confirm_password):
+            status= "Your passwords don't match!"
+        else:
+            status= "Signup done successfully!"
+    return render(request,'zing_it/signup.html',{"form":form,"status":status})
     
