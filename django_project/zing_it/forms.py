2cd6d25f802a934dc1c3218f35a00754adef4d1e
Your Name <you@example.com>
2020-06-07 00:11:01 +0500

    initial commit

--- django_project/zing_it/forms.py
+++ django_project/zing_it/forms.py
@@ -1,10 +1,13 @@
 from django import forms
+from django.contrib.auth.models import User
 
 class Signup(forms.Form):
     full_name = forms.CharField(required=True,max_length=70)
     email = forms.EmailField(required=True)
     password = forms.CharField(widget=forms.PasswordInput,required=True,min_length=5)
     confirm_password = forms.CharField(widget=forms.PasswordInput,required=True,min_length=5)
+
+
 
 
 class Login(forms.Form):
--- django_project/zing_it/views.py
+++ django_project/zing_it/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render
 from django.http import HttpResponse
 import datetime
 from django.http import Http404
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate
 
 from .forms import Signup,Login
 
@@ -56,15 +58,25 @@ def playlist(request,id):
 
 def signup(request):
     form = Signup(request.POST or None)
-    status= " "
     if form.is_valid():
         password= form.cleaned_data.get("password")
         confirm_password = form.cleaned_data.get("confirm_password")
+        name = form.cleaned_data.get("full_name")
+        email = form.cleaned_data.get("email")
+        print(email)
         if(password!= confirm_password):
-            status= "Your passwords don't match!"
+            return render(request,'zing_it/signup.html',{"form":form,"status":"Your passwords don't match!"})
         else:
-            status= "Signup done successfully!"
-    return render(request,'zing_it/signup.html',{"form":form,"status":status})
+            print("this is:",email)
+            try:
+                user= User.objects.get(email=email)
+                return render(request,'zing_it/signup.html',{"form":form,"status":"This email already exists in the system! Please log in instead."})
+            except Exception as e:
+                print(e)
+                new_user = User.objects.create_user(username= name, email= email,password= password)
+                new_user.save()
+                return render(request,'zing_it/signup.html',{"form":form,"status":"Signed up Successfully!"})
+    return render(request,'zing_it/signup.html',{"form":form})
 
 def login(request):
     form = Login(request.POST or None)
