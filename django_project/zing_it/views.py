c3e9ce82b4c5268db22b455f41f1b9ec045974a9
Your Name <you@example.com>
2020-06-11 15:15:33 +0500

    initial commit

--- django_project/zing_it/views.py
+++ django_project/zing_it/views.py
@@ -5,7 +5,9 @@ from django.http import Http404
 from django.contrib.auth.models import User
 from django.contrib.auth import authenticate
 
-from .forms import Signup,Login
+from .forms import Signup,Login, Edit
+
+from .models import Song, Playlist
 
 # Create your views here.
 
@@ -30,31 +32,87 @@ users = [
         ]
 
 
+
+
 def home(request):
-    
-    # music_types=['Pop','Rock','R&B','Soul & Funk','Blues','Reggae','Soundtracks','Dance & EDM','Rap', 'Asian Music','Jazz','Kpop','Metal','Electronic','Classical']
-    return render(request,'zing_it/home.html',{"my_playlists":my_playlists})
+
+    new_playlist= Playlist.objects.all()
+    return render(request,'zing_it/home.html',{"my_playlists":new_playlist})
 
 
 def about(request):
+    thankyou_next= Song.objects.create(track="thank u, next",artist="Ariana Grande",album="thank u, next",length="3:27",playlist_id=1)
+    one_kiss_next= Song.objects.create(track="One Kiss, next",artist="Dua Lipa, Calvin Harris",album="One Kiss",length="3:34",playlist_id=1)
+    better_now= Song.objects.create(track="Better Now",artist="Post malone",album="beerbongs & bentleys",length="3:51",playlist_id=1)
+    the_middle= Song.objects.create(track="The Middle",artist="Grey,Marren Morris, ZEDD",album="The Middle",length="3:04",playlist_id=1)
+    love_lies= Song.objects.create(track="Love Lies",artist="Normani, Khalid",album="Love Lies",length="3:21",playlist_id=2)
+    rise= Song.objects.create(track="Rise",artist="Jack & Jack, Jonas Blue",album="Blue",length="3:14",playlist_id=2)
+
+    car_playlist= Playlist.objects.create(name="Car Playlist",number_of_songs=4)
+    coding_playlist= Playlist.objects.create(name="Coding Playlist",number_of_songs=2)
+
+    try:
+        thankyou_next.save()
+        one_kiss_next.save()
+        better_now.save()
+        the_middle.save()
+        love_lies.save()
+        rise.save()
+        car_playlist.save()
+        coding_playlist.save()
+    except Exception as e: 
+        print(e)
+
+    
     return HttpResponse("""<h1>About Us:</h1><p>With Zing, you can easily find the music of your choice and easily share it with other people. You can also browse through the collections of friends, artists, and celebrities, or create a playlist of your own.
       Soundtrack your life with Zing. Subscribe or listen for free.</p>""")
 
 def playlist(request,id):
     songs=[]
     playlist_name=''
-    for playlist in my_playlists:
-        if(id == playlist['id']):
-            playlist_name=playlist['name']
+
+    new_playlist= Playlist.objects.all()
+
+    for playlist in new_playlist:
+        if(id == playlist.id):
+            playlist_name=playlist.name
 
     if len(playlist_name)==0:
         raise Http404("Such playlist does not exist")
 
-    for song in my_songs:
-        if(id == song['playlist_id']):
+    new_songs= Song.objects.all()
+
+    print(new_songs)
+
+
+    for song in new_songs:
+        if(id == song.playlist_id):
             songs.append(song)
         
     return render(request,'zing_it/songs.html',{"songs":songs,"playlist_name": playlist_name})
+
+def edit(request,id):
+    form = Edit(request.POST or None)
+    if form.is_valid():
+        track= form.cleaned_data.get("track")
+        album = form.cleaned_data.get("album")
+        artist = form.cleaned_data.get("artist")
+        length = form.cleaned_data.get("length")
+        playlist_id = form.cleaned_data.get("playlist_id")
+
+        song= Song.objects.get(id=id)
+
+        song.track= track
+        song.album = album
+        song.artist = artist
+        song.length = length
+        song.playlist_id = playlist_id
+
+        song.save()
+
+        return render(request,'zing_it/edit.html',{"form":form,"status": "Your song is updated successfully!" })
+
+    return render(request,'zing_it/edit.html',{"form":form})
 
 def signup(request):
     form = Signup(request.POST or None)
